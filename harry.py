824749dcdececaec9275e5617647a5cc76596aaa
Luciferskiy <81638824+Luciferskiy@users.noreply.github.com>
2024-02-20 10:53:53 +0200

    Update harry.py

--- harry.py
+++ harry.py
@@ -1,1 +1,14 @@
+from logic import *
 
+rain = Symbol("rain")
+hagrid = Symbol("hagrid")
+dumbledore = Symbol("dumbledore")
+
+knowlegde = And(
+    Implication(Not(rain), hagrid),
+    Or(hagrid, dumbledore)
+    Not(And(hagrid, dumbledore)),
+    dumbledore,
+)
+
+print(sentence.formula())
